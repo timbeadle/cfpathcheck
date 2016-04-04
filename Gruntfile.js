ea0000ab3f95f79750872bc4290fd0e0ad4d5846
Tim Beadle <tim.beadle@lovehoney.co.uk>
2016-04-04 10:32:21 +0100

    Add basic Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+/*
+ * cfpathcheck
+ * https://github.com/timbeadle/cfpathcheck
+ *
+ * Copyright (c) 2015 Tim Beadle
+ * Licensed under the MIT license.
+ */
+
+module.exports = function (grunt) {
+	grunt.initConfig();
+
+	grunt.loadNpmTasks('grunt-release');
+};
