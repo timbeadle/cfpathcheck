295094c0a585579dbf52947c015fcc87ffbc23e1
Tim Beadle <tim.beadle@lovehoney.co.uk>
2016-11-07 18:57:52 +0000

    .eslintrc.yaml -> .eslintrc.yml & update Gruntfile ref

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		eslint: {
 			options: {
-				configFile: '.eslintrc'
+				configFile: '.eslintrc.yml'
 			},
 			src: ['lib/*.js']
 		},
