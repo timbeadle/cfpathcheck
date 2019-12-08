0cf96fc6c35722bb79d19ac2e0fd4a37a980d78f
Tim Beadle <tim@takkconsulting.co.uk>
2019-12-08 22:20:11 +0000

    Add matchAll tests

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -4,64 +4,95 @@ const { expect } = require('chai');
 const { describe, it } = require('mocha');
 const utils = require('../lib/utils');
 
-describe('containsObject', () => {
-	it('is a function', () => {
-		expect(utils.containsObject).to.be.an.instanceOf(Function);
-	});
+describe('utils', () => {
 
-	it('returns true when object is found in array', () => {
-		const needle = {
-			foo: 'bar',
-			baz: 'quux',
-		};
+	describe('containsObject', () => {
+		it('is a function', () => {
+			expect(utils.containsObject).to.be.an.instanceOf(Function);
+		});
 
-		const haystack = [
-			{
+		it('returns true when object is found in array', () => {
+			const needle = {
 				foo: 'bar',
 				baz: 'quux',
-			}
-		];
+			};
 
-		expect(utils.containsObject(needle, haystack)).to.be.true;
+			const haystack = [
+				{
+					foo: 'bar',
+					baz: 'quux',
+				}
+			];
+
+			expect(utils.containsObject(needle, haystack)).to.be.true;
+		});
+
+		it('returns false when object is not found in array', () => {
+			const needle = {
+				foo: 'bar',
+				baz: 'quux',
+			};
+
+			const haystack = [
+				{
+					bar: 'foo',
+					quux: 'baz',
+				}
+			];
+
+			expect(utils.containsObject(needle, haystack)).to.be.false;
+		});
 	});
 
-	it('returns false when object is not found in array', () => {
-		const needle = {
-			foo: 'bar',
-			baz: 'quux',
-		};
+	describe('checkIsXMLFile', () => {
+		it('is a function', () => {
+			expect(utils.checkIsXMLFile).to.be.an.instanceOf(Function);
+		});
 
-		const haystack = [
-			{
-				bar: 'foo',
-				quux: 'baz',
-			}
-		];
+		it('returns true when an XML prolog is found', () => {
+			const result = utils.checkIsXMLFile('<?xml version="1.0"?>');
 
-		expect(utils.containsObject(needle, haystack)).to.be.false;
-	});
-});
+			expect(result).to.be.true;
+		});
 
-describe('checkIsXMLFile', () => {
-	it('is a function', () => {
-		expect(utils.checkIsXMLFile).to.be.an.instanceOf(Function);
-	});
+		it('returns true when an XML content-type is found', () => {
+			const result = utils.checkIsXMLFile('<foo type="text/xml">');
 
-	it('returns true when an XML prolog is found', () => {
-		const result = utils.checkIsXMLFile('<?xml version="1.0"?>');
+			expect(result).to.be.true;
+		});
 
-		expect(result).to.be.true;
+		it('returns false when neither an XML prolog nor content-type is found', () => {
+			const result = utils.checkIsXMLFile('<issues>');
+
+			expect(result).to.be.false;
+		});
 	});
 
-	it('returns true when an XML content-type is found', () => {
-		const result = utils.checkIsXMLFile('<foo type="text/xml">');
+	describe('matchAll', () => {
+		it('is a function', () => {
+			expect(utils.matchAll).to.be.an.instanceOf(Function);
+		});
 
-		expect(result).to.be.true;
+		it('returns an array', () => {
+			const line = '\t\t\t<foo:bar x="y" />';
+			const result = utils.matchAll(line, /<([A-Za-z0-9]+):/g);
+
+			expect(result).to.be.an.instanceOf(Array);
+		});
+
+		it('finds matches when regex is global', () => {
+			const line = '\t\t\t<foo:bar x="y" /><biz:bosh />';
+			const result = utils.matchAll(line, /<([A-Za-z0-9]+):/g);
+
+			expect(result.length).to.equal(2);
+		});
+
+		it('finds one match when regex is not global', () => {
+			const line = '\t\t\t<foo:bar x="y" />';
+			const result = utils.matchAll(line, /<([A-Za-z0-9]+):/);
+
+			expect(result.length).to.equal(1);
+		});
 	});
 
-	it('returns false when neither an XML prolog nor content-type is found', () => {
-		const result = utils.checkIsXMLFile('<issues>');
-
-		expect(result).to.be.false;
-	});
 });
