15f6be0785f88621e2daf79af2c1d25f430086ec
Tim Beadle <tim@takkconsulting.co.uk>
2023-03-21 15:17:56 +0000

    Add typescript, @types/node, tsconfig, definition files

---
+++ dist/cfpathcheck.d.ts
@@ -0,0 +1,9 @@
+export function comparePrefixArrays(prefixArray1: Array<string>, prefixArray2: Array<string>, message: string, severity: string): Array<object>;
+export function readFile(filePath: string): string;
+export function checkFile(filePath: string): Array<object>;
+export function getFiles(filePath: string): Array<string>;
+export function check(filePath: string): Array<object>;
+export function formatter(violations: Array<object>, format: string): string | Array<object>;
+export function writeFile(output: string, outFile: string): void;
+export function writeOutput(output: string | Array<object>): void;
+//# sourceMappingURL=cfpathcheck.d.ts.map
---
+++ dist/utils.d.ts
@@ -0,0 +1,4 @@
+export function containsObject(targetObject: object, list: Array<object>): boolean;
+export function checkIsXMLFile(line: string): boolean;
+export function matchAll(matchString: string, regex: RegExp): RegExpExecArray | any[];
+//# sourceMappingURL=utils.d.ts.map
