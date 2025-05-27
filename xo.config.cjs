3e4f8f66a4fe9fb1e0dc0044bdd9323d54c2856e
Tim Beadle <tim@takkconsulting.co.uk>
2025-05-27 08:53:25 +0100

    Amend xo config and tweak code to match new rules

--- xo.config.cjs
+++ xo.config.cjs
@@ -3,5 +3,7 @@ module.exports = {
 		"arrow-parens": 0,
 		"object-curly-spacing": 0,
 		"n/prefer-global/process": 0,
+		"@stylistic/object-curly-spacing": ["error", "always"],
+		"@stylistic/arrow-parens": ["error", "always"],
 	}
 };
