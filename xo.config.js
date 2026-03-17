e1c7ecd225a2d6c30c041deaa497344333b04f72
Tim Beadle <tim@takkconsulting.co.uk>
2026-03-17 22:45:54 +0000

    Change to xo flat file config

--- xo.config.js
+++ xo.config.js
@@ -1,9 +1,11 @@
-module.exports = {
-	rules: {
-		"arrow-parens": 0,
-		"object-curly-spacing": 0,
-		"n/prefer-global/process": 0,
-		"@stylistic/object-curly-spacing": ["error", "always"],
-		"@stylistic/arrow-parens": ["error", "always"],
-	}
-};
+export default [
+	{
+		rules: {
+			'arrow-parens': 0,
+			'object-curly-spacing': 0,
+			'n/prefer-global/process': 0,
+			'@stylistic/object-curly-spacing': ['error', 'always'],
+			'@stylistic/arrow-parens': ['error', 'always'],
+		},
+	},
+];
